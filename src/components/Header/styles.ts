96d5ac1f5ee744f8e72d46471d9e484ad310cdc7
Guilherme Athayde <guiathayde303@gmail.com>
2023-05-10 22:22:02 -0300

    Initial commit

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,13 @@
+import { styled } from 'styled-components';
+
+export const HeaderContainer = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Logo = styled.img`
+  width: 9.25rem;
+  height: 6.125rem;
+  object-fit: cover;
+`;
---
+++ src/styles/global.ts
@@ -0,0 +1,24 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  :focus {
+    outline: 0;
+    box-shadow: 0 0 0 1px ${({ theme }) => theme.blue};
+  }
+
+  body {
+    background: ${({ theme }) => theme.background};
+    color: ${({ theme }) => theme.text};
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, textarea, button {
+    font: 400 1rem 'Nunito', sans-serif;
+  }
+`;
